68f2f5cc28b65f6e9ce5c2fc6d20c3ec8f5b8d16
jhoe <lucasmessiassilva@outlook.com>
2018-10-14 12:08:08 -0300

    meus primeiros codigos

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var express = require('express')
-var app = express()
+var app = require('./config/server')
 
+//rotas
+var rotaNoticias = require('./app/routes/noticias')
+rotaNoticias(app)
+
+var rotaHome = require('./app/routes/home')
+rotaHome(app)
+
+var rotaInclusao = require('./app/routes/formulario_inclusao_noticias')
+rotaInclusao(app)
 app.listen(3000, function(){
-    console.log("servidor rodando na porta 3000")
+    console.log("servidor rodando na porta 3000 ")
 })
