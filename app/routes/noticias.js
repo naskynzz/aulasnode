877fba0c5eea118b67ed5cb671a6b4464f007d95
jhoe <lucasmessiassilva@outlook.com>
2018-10-15 16:22:01 -0300

    bando de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,23 @@
 module.exports = function(app){
+    
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias")
-    })
-}
+        
+        var mysql = require('mysql');
+        
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : '1234',
+            database : 'aulasnode'
+            });
+        
+            connection.query('select * from jornalista', function(error, result){
+            
+                if(error){
+                    console.log(error)
+                }else{
+                    res.send({result});
+                }
+            });
+    });
+};
